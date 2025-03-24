8e1ebcbdc142104d0edd885d32b2961105be6673
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-03-24 16:35:09 -0300

    resolve role script

--- bash/set-var-github-env.sh
+++ bash/set-var-github-env.sh
@@ -2,6 +2,7 @@ echo "REPO_NAME=$1" >> $GITHUB_ENV
 echo "ROLE_NAME=GitHubActionsRole" >> $GITHUB_ENV
 echo "ACCOUNT_ID=$2" >> $GITHUB_ENV
 
-ARN="arn:aws:iam::$2:role/GitHubActionsRole-${$1#*/}"
+ARN="arn:aws:iam::$2:role/GitHubActionsRole-${1#*/}"
+
 echo "TF_VAR_ROLE_ARN=$ARN" >> $GITHUB_ENV
 echo "TF_VAR_ROLE_ARN=$ARN" >> env.txt
