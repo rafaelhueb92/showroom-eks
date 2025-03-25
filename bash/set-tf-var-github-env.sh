bf050462ed38d86ca0b11437c94ece76bc33456a
Rafael Hueb <rafaelhueb@Rafaels-MacBook-Air.local>
2025-03-24 22:20:12 -0300

    echo Sanitized

--- bash/set-tf-var-github-env.sh
+++ bash/set-tf-var-github-env.sh
@@ -2,11 +2,11 @@ VALUE=$(terraform output -raw "$2" 2>/dev/null || echo "")
 SANITIZED_VALUE=$(echo "$VALUE" | sed 's/::debug::Terraform//g')
 
 if [ -n "$SANITIZED_VALUE" ]; then
-  echo "$1=$SANITIZED_VALUE" >> env.txt
+  echo "$1=$SANITIZED_VALUE" >> $GITHUB_ENV
 else
   echo "Warning: Terraform output '$2' not found or empty"
 fi
 
 echo "***************************************************************"
 
-cat env.txt
+$GITHUB_ENV >> env.txt
